52130eec23519157bbe725b73520f599815a1ac3
Aaron Huang <aaroms9733@gmail.com>
2016-11-22 01:14:45 +0800

    Add more test

--- spec/lita/handlers/webhook_spec.rb
+++ spec/lita/handlers/webhook_spec.rb
@@ -26,7 +26,27 @@ describe Lita::Handlers::Webhook, lita_handler: true do
   describe 'Handler callback' do
     let(:request) { double }
     let(:response) { double }
-    let(:body) { double }
+    let(:body) { <<EOS
+{
+  "events": [
+      {
+        "replyToken": "reply_token",
+        "type": "message",
+        "timestamp": 1462629479859,
+        "source": {
+             "type": "user",
+             "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+         },
+         "message": {
+             "id": "325708",
+             "type": "text",
+             "text": "Hello, world"
+          }
+      }
+  ]
+    }
+EOS
+    }
     let(:signature) { double }
     let(:env) { {'HTTP_X_LINE_SIGNATURE': signature} }
 
@@ -40,9 +60,19 @@ describe Lita::Handlers::Webhook, lita_handler: true do
 
     it 'HTTP 401 when Line signature unvalidated' do
       client.stub(:validate_signature).and_return false
-      allow(response).to receive(:status=).with(401)
+      allow(response).to receive(:status=)
       subject.callback(request, response)
-      expect(response).to have_received(:status=)
+      expect(response).to have_received(:status=).with(401)
+    end
+
+    it "HTTP 200 when validate_signature" do
+      config = double
+      config.stub(:channel_secret).and_return 'secret'
+      config.stub(:channel_token).and_return 'token'
+      robot.stub_chain(:chat_service, :client).and_return(Lita::Adapters::Line::Client.new(robot, config).client)
+      Line::Bot::Client.any_instance.stub(:validate_signature => true)
+      expect(response).to receive(:status=).with(200)
+      subject.callback(request, response)
     end
   end
 end
